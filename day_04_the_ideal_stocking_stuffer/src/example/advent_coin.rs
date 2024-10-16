e8616ccaf3eebe449fb79c770b4adbc323200f73
Pavol ONDRUSEK <pavol.ondrusek@gmail.com>
2024-10-16 12:32:40 +0200

    Day 4: The Ideal Stocking Stuffer (Part 2)

--- day_04_the_ideal_stocking_stuffer/src/example/advent_coin.rs
+++ day_04_the_ideal_stocking_stuffer/src/example/advent_coin.rs
@@ -3,14 +3,14 @@ use md5::Digest;
 pub struct AdventCoin {}
 
 impl AdventCoin {
-    pub fn mine(secret_key: &str) -> u32 {
+    pub fn mine(secret_key: &str, leading_zeros: u32) -> u32 {
         let mut suffix = 1;
 
         loop {
             let number = format!("{}{}", secret_key, suffix);
             let digest = md5::compute(&number);
 
-            if Self::digest_with_zeros(&digest) {
+            if Self::digest_with_zeros(&digest, leading_zeros) {
                 break;
             }
             suffix += 1;
@@ -19,10 +19,13 @@ impl AdventCoin {
         suffix
     }
 
-    // Check if hash digest contains leading 5 zeros
-    fn digest_with_zeros(digest: &Digest) -> bool {
-        // Check if the first 5 hexadecimal digits (20 bits) are zero
-        digest[0] == 0 && digest[1] == 0 && digest[2] & 0xF0 == 0
+    // Check if hash digest contains leading zeros
+    fn digest_with_zeros(digest: &Digest, leading_zeros: u32) -> bool {
+        match leading_zeros {
+            5 => digest[0] == 0 && digest[1] == 0 && digest[2] & 0xF0 == 0,
+            6 => digest[0] == 0 && digest[1] == 0 && digest[2] == 0,
+            _ => panic!("Unsupported leading zeros {}", leading_zeros),
+        }
     }
 }
 
@@ -33,21 +36,25 @@ mod tests {
     #[test]
     #[ignore] // Long running test
     fn test_mine() {
-        assert_eq!(AdventCoin::mine("abcdef"), 609043);
-        assert_eq!(AdventCoin::mine("pqrstuv"), 1048970);
+        assert_eq!(AdventCoin::mine("abcdef", 5), 609043);
+        assert_eq!(AdventCoin::mine("pqrstuv", 5), 1048970);
     }
 
     #[test]
     fn test_digest_with_zeros() {
-        assert!(AdventCoin::digest_with_zeros(&md5::compute("abcdef609043")));
-        assert!(AdventCoin::digest_with_zeros(&md5::compute(
-            "pqrstuv1048970"
-        )));
+        assert!(AdventCoin::digest_with_zeros(
+            &md5::compute("abcdef609043"),
+            5
+        ));
+        assert!(AdventCoin::digest_with_zeros(
+            &md5::compute("pqrstuv1048970"),
+            5
+        ));
     }
 
     #[test]
     fn test_digest_with_non_zeros() {
-        assert!(!AdventCoin::digest_with_zeros(&md5::compute("abcdef1")));
-        assert!(!AdventCoin::digest_with_zeros(&md5::compute("pqrstuv1")));
+        assert!(!AdventCoin::digest_with_zeros(&md5::compute("abcdef1"), 5));
+        assert!(!AdventCoin::digest_with_zeros(&md5::compute("pqrstuv1"), 5));
     }
 }
--- day_04_the_ideal_stocking_stuffer/src/main.rs
+++ day_04_the_ideal_stocking_stuffer/src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     println!("--- Day 4: The Ideal Stocking Stuffer ---");
     println!();
 
-    println!("Part 1: {}", AdventCoin::mine("yzbqklnj"));
+    println!("Part 1: {}", AdventCoin::mine("yzbqklnj", 5));
+    println!("Part 2: {}", AdventCoin::mine("yzbqklnj", 6));
 }
