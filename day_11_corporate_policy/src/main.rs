8d0d61b00475d7dcc4cfa94e9c243f70bd3aa30f
Pavol ONDRUSEK <pavol.ondrusek@gmail.com>
2024-10-23 18:29:46 +0200

    Day 11: Corporate Policy (Part 2)

--- day_11_corporate_policy/src/main.rs
+++ day_11_corporate_policy/src/main.rs
@@ -6,8 +6,11 @@ fn main() {
     println!("--- Day 11: Corporate Policy ---");
     println!();
 
-    println!(
-        "Part 1: New password: {}",
-        Password::new("hepxcrrq").find_next_valid_password().get()
-    );
+    let original_pwd = Password::new("hepxcrrq");
+
+    let next_pwd = original_pwd.find_next_valid_password();
+    println!("Part 1: Next password: {}", next_pwd.get());
+
+    let next_next_pwd = next_pwd.find_next_valid_password();
+    println!("Part 2: Next password: {}", next_next_pwd.get());
 }
