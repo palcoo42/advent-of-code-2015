c9d1eef7a7969a712ec00c13e91850d25d9432fa
Pavol ONDRUSEK <pavol.ondrusek@gmail.com>
2024-10-24 20:26:57 +0200

    Day 13: Knights of the Dinner Table (Part 2)

--- day_13_knights_of_the_dinner_table/src/main.rs
+++ day_13_knights_of_the_dinner_table/src/main.rs
@@ -20,7 +20,22 @@ fn main() {
     });
 
     println!(
-        "Part 1: Best happiness: {}",
+        "Part 1: Highest happiness: {}",
+        table.seat_persons_with_maximum_happiness()
+    );
+
+    let input_file = get_project_root().join("resources").join("input_me.txt");
+    let table = Reader::read_table(&input_file).unwrap_or_else(|err| {
+        eprintln!(
+            "Failed to read input file '{}'with error '{}'",
+            input_file.to_str().unwrap(),
+            err
+        );
+        exit(1);
+    });
+
+    println!(
+        "Part 2: Highest happiness: {}",
         table.seat_persons_with_maximum_happiness()
     );
 }
