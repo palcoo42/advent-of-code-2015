53bbe4ee03a5023f38c66decc74000ce61c37f86
palcoo42 <pavol.ondrusek@gmail.com>
2025-07-15 09:01:56 +0200

    Speed up Day 04 by comparing MD5 raw bytes instead of String conversions

--- day_04_the_ideal_stocking_stuffer/src/puzzle/solution.rs
+++ day_04_the_ideal_stocking_stuffer/src/puzzle/solution.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
+use std::fmt::Write;
 
+use md5::Digest;
 use puzzler::puzzler::puzzle::Puzzle;
 
 pub struct Solution {}
@@ -28,23 +30,52 @@ impl Solution {
     }
 
     fn find_md5(secret_key: &str, zeros_count: usize) -> usize {
-        // Build up leading zeros
-        let zeros = "0".repeat(zeros_count);
-
         // Start with number 0
         let mut number = 0;
 
+        // Reusable buffer to avoid format!() in every loop which allocates a new String
+        // Note: Magic number 12 is an estimation of maximum number, i.e., 999_999_999_999
+        let mut data = String::with_capacity(secret_key.len() + 12);
+
         // Loop until we find digest with leading zeros
         loop {
-            let data = format!("{secret_key}{number}");
+            // Fill in data for MD5
+            data.clear();
+            data.push_str(secret_key);
+            write!(&mut data, "{number}").unwrap();
+
+            // Compute digest
             let digest = md5::compute(&data);
 
-            if format!("{digest:x}").starts_with(&zeros) {
+            // Check for a solution
+            if Self::all_zeros(&digest, zeros_count) {
                 return number;
             }
 
             number += 1;
         }
+    }
+
+    // Compare raw bytes to avoid conversions to String
+    fn all_zeros(digest: &Digest, zeros_count: usize) -> bool {
+        let bytes = &digest.0;
+
+        // MD5 digest is 16 byte hash, each digit is 4 bits. If zeros_count is 5, we need to
+        // check 5 x 4 bits, i.e. 2 bytes and one half byte.
+        let target_bytes = zeros_count / 2;
+        let extra_half_byte = zeros_count % 2;
+
+        // Check full bytes
+        if bytes.iter().take(target_bytes).any(|b| *b != 0) {
+            return false;
+        }
+
+        // Check possible last half byte
+        if extra_half_byte > 0 && bytes[target_bytes] >> 4 != 0 {
+            return false;
+        }
+
+        true
     }
 }
 
